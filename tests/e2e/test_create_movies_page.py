4fdcfb426584aaa41356871733905b260910ac89
camerron <97686122+cgembala@users.noreply.github.com>
2022-10-25 15:39:20 -0400

    fixed tests

--- tests/e2e/test_create_movies_page.py
+++ tests/e2e/test_create_movies_page.py
@@ -8,7 +8,7 @@ def test_create_movies_page(test_app: FlaskClient):
     
     assert b'<h1 class="mb-5">Create Movie Rating</h1>' in response_data
     assert b'<p class="mb-3">Create a movie rating below</p>' in response_data
-    assert b'<input type="text" id="name" name="name" autocomplete="off"><br>' in response_data
-    assert b'<input type="text" id="director" name="director" autocomplete="off"><br>' in response_data
-    assert b'<select id="rating" name="rating">' in response_data
+    assert b'<label for="name">Movie Title:</label>' in response_data
+    assert b'<label for="director">Movie Director:</label>' in response_data
+    assert b'<label for="rating">Movie Rating:</label>' in response_data
     
--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -1,14 +1,16 @@
 # TODO: Feature 2
-from src.repositories.movie_repository import MovieRepository
+from src.repositories.movie_repository import get_movie_repository
 from src.models.movie import Movie
+
+movie_repository = get_movie_repository()
 
 def test_create_movie():
     
-    movie = MovieRepository()
-    movie.create_movie('13 hours', 'Michael Bay', 5)
-    assert len(movie.get_all_movies()) == 1
-    movie.create_movie('Iron Man', 'JOn Favreau', 5)
-    assert len(movie.get_all_movies()) == 2
+    
+    movie_repository.create_movie('13 hours', 'Michael Bay', 5)
+    assert len(movie_repository.get_all_movies()) == 1
+    movie_repository.create_movie('Iron Man', 'JOn Favreau', 5)
+    assert len(movie_repository.get_all_movies()) == 2
     
 def test_movie_model():
     movie = Movie('Thor: Love and Thunder', 'Taika Waititi', 3)
