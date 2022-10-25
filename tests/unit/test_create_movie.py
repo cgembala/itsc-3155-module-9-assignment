8ad5dc93e57de52ca0b986d01455aa6643c06e31
camerron <97686122+cgembala@users.noreply.github.com>
2022-10-25 12:14:42 -0400

    Update test_create_movie.py

--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -1,6 +1,19 @@
 # TODO: Feature 2
 from src.repositories.movie_repository import MovieRepository
-# create test cases
+from src.models.movie import Movie
+
 def test_create_movie():
-    # create a movie
-    movies = MovieRepository()
+    
+    movie = MovieRepository()
+    movie.create_movie('13 hours', 'Michael Bay', 5)
+    assert len(movie.get_all_movies()) == 1
+    movie.create_movie('Iron Man', 'JOn Favreau', 5)
+    assert len(movie.get_all_movies()) == 2
+    
+def test_movie_model():
+    movie = Movie('Thor: Love and Thunder', 'Taika Waititi', 3)
+
+    assert type(movie) == Movie
+    assert movie.title == 'Thor: Love and Thunder'
+    assert movie.director == 'Taika Waititi'
+    assert movie.rating == 3
